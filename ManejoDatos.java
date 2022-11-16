3ebd1ef1951a96ea55b86a91df5fc25228813bf6
BrandonReyes0609 <101024720+BrandonReyes0609@users.noreply.github.com>
2022-11-15 21:26:12 -0600

    Update ManejoDatos.java

--- ManejoDatos.java
+++ ManejoDatos.java
@@ -1,10 +1,18 @@
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class ManejoDatos{
     private ArrayList<FichaMedica> pacientes;
+    private ArrayList<String> listadoDatos;
+    private ArrayList<String> list1;
+
+    Archivo miArchivo = new Archivo("./stadoPaciente.csv");
 
     public ManejoDatos(){
         pacientes = new ArrayList<FichaMedica>();
+        listadoDatos = new ArrayList<String>();
+        
     }
     
     /** 
@@ -18,9 +26,19 @@ public class ManejoDatos{
      * @param datoIMC
      * @param estatura;
      */
-    public void agregarPacientes(String nombre,String apellidos,int edad,int peso,String realizaEjercicio,int numComida,String sexo,float datoIMC,float estatura){
+    public void agregarPacientes(String nombre,String apellidos,int edad,double peso,String realizaEjercicio,int numComida,String sexo,double datoIMC,double estatura){
         FichaMedica nuevFichaMedica = new FichaMedica(nombre, apellidos, edad, peso, realizaEjercicio, numComida, sexo, datoIMC, estatura);
         this.pacientes.add(nuevFichaMedica);
+        //Integer.toString(edad)
+        String cadenaEscrbribir = nombre+","+Integer.toString(edad)+","+Double.toString(peso)+","+realizaEjercicio+","+Integer.toString(numComida)+","+sexo+","+Double.toString(datoIMC)+","+Double.toString(estatura)+"\n";
+
+        System.out.println(cadenaEscrbribir);
+        // aqui podemos mandar los datos para guardar
+        //miArchivo.escribirArchivo("nombre,edad,color,genero,raza\nlaika,8,negro,hembra,labrador\nlisa,0,manto negro,hembra,pastor alemán\n");
+        miArchivo.escribirArchivo(cadenaEscrbribir);
+
+        //miArchivo.escribirArchivo("nombre,edad,color,genero,raza\nlaika,8,negro,hembra,labrador\nlisa,0,manto negro,hembra,pastor alemán\n");
+
     }
 
     
@@ -52,21 +70,24 @@ public class ManejoDatos{
     }
 
 
+    public ArrayList<FichaMedica> enviarPacientes() {
+        return this.pacientes;
+    }
+
     public String pacientesFichaMedica(){
-        int i = 1; 
+        int i = 0; 
         String resultados = "";
         if(pacientes.size()>0){
-            while (i<=pacientes.size()){
+            while (i<pacientes.size()){
                 resultados = resultados + "\n------ Paciente " + i + " ------\n";
-                resultados = resultados + "Nombre: "+ pacientes.get(i-1).getNombre()+"\n";
-                resultados = resultados + "Apeliido: "+ pacientes.get(i-1).getApellidos()+"\n";
-                resultados = resultados + "Edad: "+ pacientes.get(i-1).getEdad()+" anios"+"\n";
-                resultados = resultados + "Peso: "+ pacientes.get(i-1).getPeso()+" Kg"+"\n";
-                resultados = resultados + "Realiza Ejercicio: "+ pacientes.get(i-1).getRealizaEjercicio()+"\n";
-                resultados = resultados + "Sexo o Genero: "+ pacientes.get(i-1).getSexo()+"\n";
-                resultados = resultados + "Numero de Comida: "+ pacientes.get(i-1).getNumComida()+"\n";
-                resultados = resultados + "Dato IMC: "+ pacientes.get(i-1).getDatoIMC()+"\n";
-                resultados = resultados + "Estatura: "+ pacientes.get(i-1).getEstatura()+" metros"+"\n";
+                resultados = resultados + ""+ pacientes.get(i).getNombre()+" "+pacientes.get(i).getApellidos()+"\n";
+                resultados = resultados + "Edad: "+ pacientes.get(i).getEdad()+"\n";
+                resultados = resultados + "Peso: "+ pacientes.get(i).getPeso()+"\n";
+                resultados = resultados + "Realiza Ejercicio: "+ pacientes.get(i).getRealizaEjercicio()+"\n";
+                resultados = resultados + "Sexo o Genero: "+ pacientes.get(i).getSexo() +"\n";
+                resultados = resultados + "Numero de Comida: "+ pacientes.get(i).getNumComida()+"\n";
+                resultados = resultados + "Dato IMC: "+ pacientes.get(i).getDatoIMC()+"\n";
+                resultados = resultados + "Estatura: "+ pacientes.get(i).getEstatura()+"\n";
 
                 resultados = resultados + "-----------------------------\n";
                 i++;
@@ -82,17 +103,17 @@ public class ManejoDatos{
         String estadisticas = "";
         if(pacientes.size()>0){
             estadisticas = "Estadisticas de los datos recolectados\n--------------------------------------------------------\n";
-            float sumaimc = 0;
+            double sumaimc = 0;
             int i = 0;
             for (FichaMedica ficha : pacientes){
                 sumaimc = sumaimc + ficha.getDatoIMC();
                 i++;
             }
-            float promedioimc = sumaimc/i;
+            double promedioimc = sumaimc/i;
             estadisticas = "IMC Promedio: "+promedioimc;
-            float sumaimch = 0;
-            float sumaimcm = 0;
-            float sumaimco = 0;
+            double sumaimch = 0;
+            double sumaimcm = 0;
+            double sumaimco = 0;
             int ih = 0;
             int im = 0;
             int io = 0;
@@ -105,10 +126,14 @@ public class ManejoDatos{
                     sumaimcm = sumaimcm + ficha.getDatoIMC();
                     im++;
                 }
+                if(ficha.getSexo().equals("otro") || ficha.getSexo().equals("Otro")){
+                    sumaimco = sumaimco + ficha.getDatoIMC();
+                    io++;
+                    }
             }
-            float promedioimch = sumaimc/ih;
-            float promedioimcm = sumaimc/im;
-            float promedioimco = sumaimc/io;
+            double promedioimch = sumaimc/ih;
+            double promedioimcm = sumaimc/im;
+            double promedioimco = sumaimc/io;
             estadisticas = estadisticas + "\nIMC Promedio para 'Hombres': " + promedioimch + "\nIMC Promedio para 'Mujeres': " + promedioimcm + "\nIMC Promedio para 'Otro': " + promedioimco + "\n";
             estadisticas = estadisticas + "\nTotal de fichas medicas registradas:\n'Hombres: "+ ih +"'Mujeres': "+ im + "'Otro': "+ io + "'Total': " + i;
         }
@@ -117,4 +142,68 @@ public class ManejoDatos{
         }
         return estadisticas;
     }
+
+    public String recomendacionesString(){
+        /*
+         * Docuemntación de apoyo
+         * https://www.cdc.gov/healthyweight/spanish/assessing/bmi/adult_bmi/metric_bmi_calculator/bmi_calculator.html
+         */
+        int i = 0;
+        String resultados = "";
+        if(pacientes.size()>0){
+            while (i<pacientes.size()){
+                resultados = resultados + "\n------ Paciente " + i + " ------\n";
+                resultados = resultados + ""+ pacientes.get(i).getNombre()+" "+pacientes.get(i).getApellidos()+"\n";
+                resultados = resultados + "Edad: "+ pacientes.get(i).getEdad()+"\n";
+                resultados = resultados + "Peso: "+ pacientes.get(i).getPeso()+"\n";
+                resultados = resultados + "Realiza Ejercicio: "+ pacientes.get(i).getRealizaEjercicio()+"\n";
+                resultados = resultados + "Sexo o Genero: "+ pacientes.get(i).getSexo() +"\n";
+                resultados = resultados + "Numero de Comida: "+ pacientes.get(i).getNumComida()+"\n";
+                resultados = resultados + "Dato IMC: "+ pacientes.get(i).getDatoIMC()+"\n";
+                resultados = resultados + "Estatura: "+ pacientes.get(i).getEstatura()+"\n";
+
+                resultados = resultados + "-----------------------------\n";
+                i++;
+
+            }
+        }
+        else {
+            resultados = "\nNo hay pacientes para mostrar.";
+        }  
+        return resultados;
+
+    }
+
+    
+    public void LeerDatosNuevos(){
+        //System.out.println(miArchivo.leerArchivoString());
+        //<ArrayList> listado = miArchivo.leerArchivoString();
+        String listadoA =  miArchivo.leerArchivoString();
+        //listadoDatos = Arrays.asList(listadoA.split("\\s+"));
+        this.list1 = new ArrayList<String>(Arrays.asList(listadoA.split(",")));
+        System.out.println(list1);
+
+        int i = 0;
+        System.out.println(list1.size());
+
+        while(i < list1.size()){
+            System.out.println(list1.get(i));
+            //System.out.println((i));
+            i++;
+        }
+
+        //String stringDatos = miArchivo.leerArchivoString();//.split("\\s+");
+
+        //System.out.println("stringDatos: "+stringDatos);
+        //FichaMedica nuevFichaMedica = new FichaMedica(nombre, apellidos, edad, peso, realizaEjercicio, numComida, sexo, datoIMC, estatura);
+        //System.out.println((miArchivo.leerArchivo()).getClass().getSimpleName());
+        
+        
+    }
+
+    public ArrayList<String> getListadoPersonas() {
+        return this.list1;
+    }
+
+    
 }
