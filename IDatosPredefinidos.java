fb9196c362e5953937591eacc0e7559dbe957dc2
WiCald <99890584+WiCald@users.noreply.github.com>
2022-11-15 21:22:58 -0600

    Add files via upload

---
+++ IDatosPredefinidos.java
@@ -0,0 +1,7 @@
+public interface IDatosPredefinidos {
+    public String mensaje1();
+    public String mensaje2();
+    public String mensaje3();
+    public String mensaje4();
+
+}
---
+++ Mesaje.java
@@ -0,0 +1,18 @@
+public class Mesaje implements IDatosPredefinidos {
+
+    public String mensaje1(){
+        return "se encuentra dentro del rango de peso insuficiente";
+    }
+
+    public String mensaje2(){
+        return "se encuentra dentro del rango de peso normal o saludable.";
+    }
+
+    public String mensaje3(){
+        return "se encuentra dentro del rango de sobrepeso";
+    }
+
+    public String mensaje4(){
+        return "se encuentra dentro del rango de obesidad";
+    }
+}
