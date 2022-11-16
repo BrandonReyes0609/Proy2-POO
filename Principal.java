fbd470919a5b36a355f8e142922ada206e934874
BrandonReyes0609 <101024720+BrandonReyes0609@users.noreply.github.com>
2022-11-15 21:25:31 -0600

    Acutalización de datos

--- Principal.java
+++ Principal.java
@@ -1,12 +1,12 @@
-import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.lang.Integer;
 
 public class Principal {
     public static void main(String[] args){
 
         ManejoDatos listadoPacientes = new ManejoDatos();
         Scanner teclado = new Scanner(System.in);
-        Archivo miArchivo = new Archivo();
         System.out.println("Bienvenido al programa");
         String menu = "\n--------------------------------------------------------\n| Seleccione alguna de las siguientes opciones:        |\n| (1) Ingreso nuevo paciente                           |\n| (2) Ver datos paciente                               |\n| (3) Recomendaciones paciente                         |\n| (4) Estadistica                                      |\n| (5) Guardar y Salir                                  |\n| (6) Leer Archivo                                     |\n--------------------------------------------------------\nOpcion a elegir: ";
         System.out.println(menu);
@@ -16,30 +16,36 @@ public class Principal {
             switch(op){
                 case 1:{
                     try{
-                    System.out.println("\nSelecciono Opcion 1 \n----Ingreso de nuevo paciente----");
-                    System.out.println("Ingrese el nombre del paciente: ");
-                    String nombre = teclado.nextLine();
-                    System.out.println("Ingrese el apellido: ");
-                    String apellidos = teclado.nextLine();
-                    System.out.println("Ingrese el edad: ");
-                    int edad = teclado.nextInt();
-                    teclado.nextLine();
-                    System.out.println("Ingrese el peso en Kg: ");
-                    int peso = teclado.nextInt();
-                    teclado.nextLine();
-                    System.out.println("Ingrese si realiza ejercicio (si/no): ");
-                    String realizaEjercicio = teclado.nextLine();
-                    System.out.println("Ingrese el numero de comidas al dia: ");
-                    int numComida = teclado.nextInt();
-                    System.out.println("Ingrese el sexo o genero (hombre/mujer): ");
-                    teclado.nextLine();
-                    String sexo = teclado.nextLine();
-                    System.out.println("Ingrese la estatura en Metros: ");
-                    float estatura = teclado.nextFloat();
-                    teclado.nextLine();
-                    float datoIMC = peso / (estatura*estatura);
-                    
-                    listadoPacientes.agregarPacientes(nombre, apellidos, edad, peso, realizaEjercicio, numComida, sexo, datoIMC, estatura);
+                        System.out.println("Selecciono Opcion 1 \n----Ingreso de nuevo paciente----");
+                        System.out.println("Ingrese el nombre del paciente: ");
+                        String nombre = teclado.nextLine();
+                        System.out.println("Ingrese el apellido: ");
+                        String apellidos = teclado.nextLine(); ;
+                        System.out.println("Ingrese el edad: ");
+                        int edad = teclado.nextInt();
+                        teclado.nextLine();
+                        System.out.println("Ingrese el peso en Lb: ");
+                        double peso = teclado.nextDouble();
+                        teclado.nextLine();
+                        System.out.println("Ingrese si realiza ejercicio \n (1) Estilo de Vida Sedentario \n (2) Estilo de Vida Ligeramente activo \n (3) Estilo de Vida Activo o Moderadamente activo \n (4) Estilo de Vida Vigoroso: ");
+                        String realizaEjercicio = teclado.nextLine();
+                        System.out.println("Ingrese el numero de comidas al dia: ");
+                        int numComida = teclado.nextInt();
+                        System.out.println("Ingrese el sexo o genero (hombre/mujer): ");
+                        teclado.nextLine();
+                        String sexo = teclado.nextLine();
+                        System.out.println("Ingrese la estatura en 'cm': ");
+                        double estaturaCM = teclado.nextDouble();
+                        double estaturaM = estaturaCM*0.01; 
+                        double pesoKG = peso*1/2.20462; 
+                        
+    
+                        teclado.nextLine();
+                        
+                        double datoIMC = pesoKG / (estaturaM*estaturaM);
+                        System.out.println("El IMC es: "+datoIMC);
+                        
+                        listadoPacientes.agregarPacientes(nombre, apellidos, edad, peso, realizaEjercicio, numComida, sexo, datoIMC, estaturaCM);
                     }
                     catch (Exception e){
                         teclado.next();
@@ -57,28 +63,41 @@ public class Principal {
                     break;
                 }
                 case 3:{
-                    System.out.println("\nSelecciono Opcion 3\n----Recomendaciones paciente----");
-                    break;
+                 System.out.println("Selecciono Opcion 3\n----Recomendaciones paciente----");
+
+                    // ver recomndaciones 
+                    //Recomendaciones notaMedicaRe = new Recomendaciones();
+                    
+                    //System.out.println(listadoPacientes.enviarPacientes().getClass().getSimpleName());
+                    
+                    //System.out.println(listadoPacientes.getPacientes());
+                    System.out.println("Ingrese el nombre a buscar");
+                    String nombreBuscar = teclado.nextLine();
+                    
+                    Recomendaciones NotaRDocRecomentacion = new Recomendaciones();
+                    //1NotaRDocRecomentacion.AgregarListadoPersonas(listadoPacientes.estadistica());
+                   NotaRDocRecomentacion.AgregarListadoPersonas(listadoPacientes.getPacientes(),nombreBuscar);
+
+
+                    System.out.println("Hola: "+NotaRDocRecomentacion.toString());
+                    
+                    System.out.println(NotaRDocRecomentacion.CalculadorString());
                 }
                 case 4:{
-                    System.out.println("\nSelecciono Opcion 4\n----Ver estadisticas----");
+                    System.out.println("Selecciono Opcion 4\n----Ver estadisticas----");
                     System.out.println(listadoPacientes.estadistica());
+
                     break;
                 }case 5:{
                     op = 5;
                     System.out.println("Guardar y cerrar archivo");
-                    listadoPacientes.getPacientes();
-
-                    ArrayList<String> lineas = miArchivo.leerArchivo();
-                    for (String linea : lineas) {
-                        System.out.println(linea);
-                    }
-                    miArchivo.leerArchivoString();
                     
                     break;
                 }case 6:{
-                    System.out.println("Leer datos paciente");
+                    System.out.println("Leer datos pasiente");
                     listadoPacientes.getPacientes();
+                    listadoPacientes.LeerDatosNuevos();
+                    listadoPacientes.getListadoPersonas();
                 }
 
                     
