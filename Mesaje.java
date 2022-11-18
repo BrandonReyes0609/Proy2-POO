9d8d1514cbde3d14754cc0c260f0c730f804a36d
BrandonReyes0609 <101024720+BrandonReyes0609@users.noreply.github.com>
2022-11-17 21:36:56 -0600

    Update Mesaje.java

--- Mesaje.java
+++ Mesaje.java
@@ -15,4 +15,11 @@ public class Mesaje implements IDatosPredefinidos {
     public String mensaje4(){
         return "se encuentra dentro del rango de obesidad";
     }
+    /*
+ * Audrey Samantha Bhor López - 22545
+    Brandon Javier Reyes Morales - 22992
+    Wilson Alejandro Calderón A. - 22018
+    Jose Angel Morales Farfan - 22689
+
+ */
 }
