05b760f8946d8cfcfa4da111f864b9bfc55541cf
BrandonReyes0609 <101024720+BrandonReyes0609@users.noreply.github.com>
2022-11-17 21:35:59 -0600

    Update IDatosPredefinidos.java

--- IDatosPredefinidos.java
+++ IDatosPredefinidos.java
@@ -3,5 +3,11 @@ public interface IDatosPredefinidos {
     public String mensaje2();
     public String mensaje3();
     public String mensaje4();
+/*
+ * Audrey Samantha Bhor López - 22545
+    Brandon Javier Reyes Morales - 22992
+    Wilson Alejandro Calderón A. - 22018
+    Jose Angel Morales Farfan - 22689
 
+ */
 }
